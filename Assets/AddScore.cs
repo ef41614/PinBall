5e5f85b993c25e0a5ff06ebd3975db740da6db83
Atsushi.Iida <ef41614@gmail.com>
2018-04-05 13:34:05 +0900

    PinBallプロジェクト 1回目のコミット
    Lesson5_発展課題：スマホで操作可。タップ場所により左右フリッパー切り替わり実装。

---
+++ Assets/AddScore.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddScore : MonoBehaviour {
+	public int tokuten =0; //各得点の変数
+	private int score = 0; //スコア計算用変数
+	private GameObject ScoreText;
+
+	void Start() {
+		score   = 0;
+		//シーン中のScoreTextオブジェクトを取得
+		this.ScoreText = GameObject.Find("ScoreText");
+		SetScore(0);   //初期スコアを代入して表示
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+	//それぞれのタグごとに得点を加点
+	void OnCollisionEnter( Collision collision ) {
+		string yourTag  = collision.gameObject.tag;
+
+		if (yourTag == "SmallStarTag") {
+			tokuten = 10;
+		} else if (yourTag == "LargeStarTag") {
+			tokuten = 100;
+		} else if (yourTag == "SmallCloudTag") {
+			tokuten = 20;
+		} else if (yourTag == "LargeCloudTag") {
+			tokuten = 30;
+		} else {
+			tokuten = 0;
+		}
+		SetScore(tokuten);
+	}
+
+
+		void SetScore(int val){
+		score += val;
+		ScoreText.GetComponent<Text> ().text = "得点："+score;
+		Debug.Log (score);
+		}
+	}
---
+++ Assets/GamenTouch.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamenTouch : MonoBehaviour {
+	public Vector2 startPos;
+	public Vector2 direction;
+	public bool directionChosen;
+	public bool tapSide = true; //タップした側が左側（true）か右側（false）か
+
+	//HingiJointコンポーネントを入れる
+	private HingeJoint myHingeJoint;
+
+	//初期の傾き
+	private float defaultAngle = 20;
+	//弾いた時の傾き
+	private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+		//HingeJointコンポーネント取得
+		this.myHingeJoint = GetComponent<HingeJoint>();
+
+		//フリッパーの傾きを設定
+		SetAngle(this.defaultAngle);
+	}
+
+
+	void Update() {
+
+		// Track a single touch as a direction control.
+		if (Input.touchCount > 0) {
+			Touch touch = Input.GetTouch(0);
+
+			switch (touch.phase) {
+				//Began：パネルに指がついた状態
+			case TouchPhase.Began:
+				startPos = touch.position;
+				directionChosen = false;
+						//左側を押した時、左フリッパーを動かす
+				   	if (startPos.x < Screen.width*0.5 && tag == "LeftFripperTag") {
+						SetAngle (this.flickAngle);
+						//右側を押した時、左フリッパーを動かさない
+					} else if (startPos.x >= Screen.width*0.5 && tag == "LeftFripperTag") {
+						SetAngle (this.defaultAngle);
+					    //左側を押した時、右フリッパーを動かさない
+					} else if (startPos.x < Screen.width*0.5 && tag == "RightFripperTag"){
+						SetAngle (this.defaultAngle);
+						//右側を押した時、右フリッパーを動かす				
+					} else if (startPos.x >= Screen.width*0.5 && tag == "RightFripperTag"){
+						SetAngle (this.flickAngle);
+					}					
+				break;
+
+				// Moved：指が動いている状態
+			case TouchPhase.Moved:
+				direction = touch.position - startPos;
+				break;
+
+				// Stationary：パネルに指はあるけど、指が動いていない状態
+			case TouchPhase.Stationary:
+				direction = touch.position - startPos;
+				if (Input.touchCount > 1) {
+					SetAngle (this.flickAngle);	
+				}
+				break;
+
+				// Ended：指がパネルから離れた状態
+			case TouchPhase.Ended:
+				directionChosen = true;
+					SetAngle (this.defaultAngle);
+				break;
+			}
+		}
+			
+	}
+
+
+	//フリッパーの傾きを設定
+	public void SetAngle (float angle){
+		JointSpring jointSpr = this.myHingeJoint.spring;
+		jointSpr.targetPosition = angle;
+		this.myHingeJoint.spring = jointSpr;
+	}
+
+}
